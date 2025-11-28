794114ff8f5fb1aa7711679f8f3c74b281b745b3
LoweComp <terci_300@outlook.com>
2025-11-28 19:46:31 -0300

    Dev dos Fornecedores

---
+++ src/main/java/MediatorJVW/FornecedorAudio.java
@@ -0,0 +1,15 @@
+package MediatorJVW;
+
+public class FornecedorAudio extends FornecedorComponente {
+    private String status = "ESPERANDO";
+
+    public FornecedorAudio(ICoordenadorDeShow mediator) { super(mediator); }
+
+    public void iniciarInstalacao() {
+        this.status = "INSTALACAO CABEAMENTO";
+        enviarEvento("INSTALACAO_AUDIO_CONCLUIDA");
+    }
+
+    @Override
+    public String getStatus() { return "Áudio: " + status; }
+}
